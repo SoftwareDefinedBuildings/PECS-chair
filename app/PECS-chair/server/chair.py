2d3883053f473fa56f8beb3272cae8d52fcbf6b9
Sam Kumar <samkumar@berkeley.edu>
2015-06-25 05:36:30 +0000

    Stop using shared global namespace in sMAP driver to fix bug

--- app/PECS-chair/server/chair.py
+++ app/PECS-chair/server/chair.py
@@ -9,51 +9,34 @@ from twisted.internet import reactor
 # Consider data to be unknown if no FS data is received for this many seconds
 INACTIVITY_GAP = 35
 
-readings = {
-    "backh": 0,
-    "bottomh": 0,
-    "backf": 0,
-    "bottomf": 0,
-    "occupancy": 0,
-    "temperature": 0.0,
-    "humidity": 0.0
-}
+def append_to_driver(driver):
+    driver.readings = {
+        "backh": 0,
+        "bottomh": 0,
+        "backf": 0,
+        "bottomf": 0,
+        "occupancy": 0,
+        "temperature": 0.0,
+        "humidity": 0.0
+    }
 
-settings = {
-    "backh": 0,
-    "bottomh": 0,
-    "backf": 0,
-    "bottomf": 0
-}
-
-translator = {
-    "OFF": 0,
-    "ON": 100,
-    "LOW": 25,
-    "MEDIUM": 50,
-    "HIGH": 75,
-    "MAX": 100
-}
-
-backh_hist = None
-bottomh_hist = None
-backf_hist = None
-bottomf_hist = None
-occ_hist = None
-temp_hist = None
-hum_hist = None
-sdriver = None
-
-port = None
+    driver.settings = {
+        "backh": 0,
+        "bottomh": 0,
+        "backf": 0,
+        "bottomf": 0
+    }
 
 class ChairResource(Resource):
     isLeaf = True
-    def __init__(self, *args):
+    def __init__(self, driverInst, *args):
         Resource.__init__(self, *args)
+        self.driver = driverInst
         self.lastAct = time.time()
         self.lasttruevaltime = 0
         self.lasthistvaltime = 1
     def render_GET(self, request):
+        settings = self.driver.settings
         doc = {"time": self.lastAct,
             "bottomh": settings["bottomh"],
             "backh": settings["backh"],
@@ -63,10 +46,11 @@ class ChairResource(Resource):
     def render_POST(self, request):
         doc_recvd = request.content.read()
         print doc_recvd
-        print "Got JSON at port", port
+        print "Got JSON at port", self.driver.port
+        sdriver = self.driver
         doc = json.loads(doc_recvd)
         if "timestamp" in doc:
-            if sdriver is None:
+            if self.driver is None:
                 # Make sure an ACK doesn't get sent back!
                 return 'failure'
             print "HANDLING HISTORICAL POINT"
@@ -89,60 +73,48 @@ class ChairResource(Resource):
         else:
             isReading = "fromFS" in doc and doc["fromFS"]
             for key in doc:
-                if key in settings:
-                    settings[key] = doc[key]
-                if isReading and key in readings:
-                    readings[key] = doc[key]
+                if key in self.driver.settings:
+                    self.driver.settings[key] = doc[key]
+                if isReading and key in self.driver.readings:
+                    self.driver.readings[key] = doc[key]
             self.lastAct = time.time()
             if "fromFS" in doc and doc["fromFS"]:
-                print "lasttruevaltime", port
+                print "lasttruevaltime", self.driver.port
                 self.lasttruevaltime = self.lastAct
         return str(self.lastAct)
 
 class PECSChairDriver(driver.SmapDriver):
     def setup(self, opts):
-        global backh_hist
-        global bottomh_hist
-        global backf_hist
-        global bottomf_hist
-        global occ_hist
-        global temp_hist
-        global hum_hist
-        global sdriver
-        
-        sdriver = self
-    
-        self.state = readings.copy()
+        append_to_driver(self)
+        self.state = self.readings.copy()
         self.macaddr = opts.get("macaddr")
-        backh = self.add_timeseries('/backheater', '%', data_type='long')
-        bottomh = self.add_timeseries('/bottomheater', '%', data_type='long')
-        backf = self.add_timeseries('/backfan', '%', data_type='long')
-        bottomf = self.add_timeseries('/bottomfan', '%', data_type='long')
-        occ = self.add_timeseries('/occupancy', 'binary', data_type='long')
-        temp = self.add_timeseries('/temperature', 'Celsius', data_type='double')
-        hum = self.add_timeseries('/humidity', '%', data_type='double')
+        self.backh = self.add_timeseries('/backheater', '%', data_type='long')
+        self.bottomh = self.add_timeseries('/bottomheater', '%', data_type='long')
+        self.backf = self.add_timeseries('/backfan', '%', data_type='long')
+        self.bottomf = self.add_timeseries('/bottomfan', '%', data_type='long')
+        self.occ = self.add_timeseries('/occupancy', 'binary', data_type='long')
+        self.temp = self.add_timeseries('/temperature', 'Celsius', data_type='double')
+        self.hum = self.add_timeseries('/humidity', '%', data_type='double')
         
-        backh_hist = self.add_timeseries('/backheater_hist', '%', data_type='long')
-        bottomh_hist = self.add_timeseries('/bottomheater_hist', '%', data_type='long')
-        backf_hist = self.add_timeseries('/backfan_hist', '%', data_type='long')
-        bottomf_hist = self.add_timeseries('/bottomfan_hist', '%', data_type='long')
-        occ_hist = self.add_timeseries('/occupancy_hist', 'binary', data_type='long')
-        temp_hist = self.add_timeseries('/temperature_hist', 'Celsius', data_type='double')
-        hum_hist = self.add_timeseries('/humidity_hist', '%', data_type='double')
+        self.backh_hist = self.add_timeseries('/backheater_hist', '%', data_type='long')
+        self.bottomh_hist = self.add_timeseries('/bottomheater_hist', '%', data_type='long')
+        self.backf_hist = self.add_timeseries('/backfan_hist', '%', data_type='long')
+        self.bottomf_hist = self.add_timeseries('/bottomfan_hist', '%', data_type='long')
+        self.occ_hist = self.add_timeseries('/occupancy_hist', 'binary', data_type='long')
+        self.temp_hist = self.add_timeseries('/temperature_hist', 'Celsius', data_type='double')
+        self.hum_hist = self.add_timeseries('/humidity_hist', '%', data_type='double')
 
         archiver = opts.get('archiver')
-        backh.add_actuator(ChairActuator(chair=self, key="backh", archiver=archiver))
-        bottomh.add_actuator(ChairActuator(chair=self, key="bottomh", archiver=archiver))
-        backf.add_actuator(ChairActuator(chair=self, key="backf", archiver=archiver))
-        bottomf.add_actuator(ChairActuator(chair=self, key="bottomf", archiver=archiver))
+        self.backh.add_actuator(ChairActuator(chair=self, key="backh", archiver=archiver))
+        self.bottomh.add_actuator(ChairActuator(chair=self, key="bottomh", archiver=archiver))
+        self.backf.add_actuator(ChairActuator(chair=self, key="backf", archiver=archiver))
+        self.bottomf.add_actuator(ChairActuator(chair=self, key="bottomf", archiver=archiver))
 
         self.port = int(opts.get('port', 9001))
 
-        global port
-        port = self.port
         print "Setting up a chair driver with port", self.port
 
-        self.resource = ChairResource()
+        self.resource = ChairResource(self)
         self.factory = Site(self.resource)
 
     def start(self):
@@ -152,7 +124,7 @@ class PECSChairDriver(driver.SmapDriver):
 
     def poll(self):
         print "Polling a chair driver with port", self.port
-        self.state = readings.copy()
+        self.state = self.readings.copy()
         currTime = time.time()
         print currTime
         print self.resource.lasttruevaltime
